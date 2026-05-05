2ed5a9c14e17c0606d3a0edba55bad080a3024e7
Ravi Patel <ravispatel303@gmail.com>
2026-05-04 22:49:15 -0700

    missing icons

--- ecommerce-project/src/pages/OrdersPage.jsx
+++ ecommerce-project/src/pages/OrdersPage.jsx
@@ -1,6 +1,6 @@
 import { Header } from '../components/Header';
 import { Link } from 'react-router';
-import BuyAgainIcon from '../assets/images/buy-again.png';
+import BuyAgainIcon from '../assets/images/icons/buy-again.png';
 import './OrdersPage.css';
 
 export function OrdersPage() {
