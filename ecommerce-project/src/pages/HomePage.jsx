76cccc82e357f81531a62e96da8ea04b14814390
Ravi Patel <ravispatel303@gmail.com>
2026-05-08 16:29:13 -0700

    Fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -4,6 +4,13 @@ import CheckmarkIcon from '../assets/images/icons/checkmark.png';
 import './HomePage.css';
 
 export function HomePage() {
+  fetch('http://localhost:3000/api/products')
+    .then((response) => {
+      return response.json();
+    }).then((data) => {
+      console.log(data);
+    });
+
   return (
     <>
       <title>Ecommerce Project</title>
