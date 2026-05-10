6903433ff8baee99465dcc5952f648f1b0b4f88b
Ravi Patel <ravispatel303@gmail.com>
2026-05-10 12:49:50 -0700

    Lift cart up and share it between pages

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,4 +1,6 @@
+import axios from 'axios';
 import { Routes, Route } from 'react-router';
+import { useEffect, useState } from 'react';
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import { OrdersPage } from './pages/OrdersPage';
@@ -7,10 +9,18 @@ import { NotFoundPage } from './pages/NotFoundPage';
 import './App.css'
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/cart-items')
+      .then((response) => {
+        setCart(response.data);
+      });
+  }, []);
 
   return (
     <Routes>
-      <Route index element={<HomePage />} /> {/* index --> path="/" */}
+      <Route index element={<HomePage cart={cart} />} /> {/* index --> path="/" */}
       <Route path="checkout" element={<CheckoutPage />} />
       <Route path="orders" element={<OrdersPage />} />
       <Route path="tracking" element={<TrackingPage />} />
--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -4,20 +4,14 @@ import { Header } from '../components/Header';
 import CheckmarkIcon from '../assets/images/icons/checkmark.png';
 import './HomePage.css';
 
-export function HomePage() {
+export function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
 
   // below useEffect ensures that we get products from api once, regardless of any re-render of HomePage. 
   useEffect(() => {
     axios.get('/api/products')
       .then((response) => {
         setProducts(response.data);
-      });
-
-    axios.get('/api/cart-items')
-      .then((response) => {
-        setCart(response.data);
       });
   }, []);
 
