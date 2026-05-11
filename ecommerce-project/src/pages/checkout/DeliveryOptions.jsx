fee7c39121c1e1471d568d244c86abd92b298674
Ravi Patel <ravispatel303@gmail.com>
2026-05-10 21:54:14 -0700

    Separate into smaller components

---
+++ ecommerce-project/src/pages/checkout/DeliveryOptions.jsx
@@ -0,0 +1,31 @@
+import dayjs from 'dayjs';
+import { formatMoney } from '../../utils/money';
+
+export function DeliveryOptions({ deliveryOptions, cartItem }) {
+  return (
+    <div className="delivery-options">
+      <div className="delivery-options-title">
+        Choose a delivery option:
+      </div>
+      {deliveryOptions.map((deliveryOption) => {
+        return (
+          <div key={deliveryOption.id}
+            className="delivery-option">
+            <input type="radio"
+              defaultChecked={deliveryOption.id === cartItem.deliveryOptionId}
+              className="delivery-option-input"
+              name={`delivery-option-${cartItem.productId}`} />
+            <div>
+              <div className="delivery-option-date">
+                {dayjs(deliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
+              </div>
+              <div className="delivery-option-price">
+                {deliveryOption.priceCents ? `${formatMoney(deliveryOption.priceCents)} - Shipping` : 'FREE Shipping'}
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
---
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Header } from '../../components/Header';
+import { ProductsGrid } from './ProductsGrid';
+import './HomePage.css';
+
+export function HomePage({ cart }) {
+  const [products, setProducts] = useState([]);
+
+  // below useEffect ensures that we get products from api once, regardless of any re-render of HomePage. 
+  useEffect(() => {
+    axios.get('/api/products')
+      .then((response) => {
+        setProducts(response.data);
+      });
+  }, []);
+
+  return (
+    <>
+      <title>Ecommerce Project</title>
+      <link rel="icon" type="image/svg+xml" href="home-favicon.png" />
+
+      <Header cart={cart} />
+
+      <div className="home-page">
+        <ProductsGrid products={products} />
+      </div>
+    </>
+  );
+}
