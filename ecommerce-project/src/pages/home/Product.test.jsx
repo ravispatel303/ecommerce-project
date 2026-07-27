5bf6417d805cc72eaf4fc67bcaedae6dd5ae84be
Ravi Patel <ravispatel303@gmail.com>
2026-07-26 17:10:55 -0700

    exercise 9 all done

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -9,6 +9,7 @@ vi.mock('axios');
 describe('Product component', () => {
   let product;
   let loadCart;
+  let user;
 
   // beforeEach() will run before each test. It is call Test Hook
   beforeEach(() => {
@@ -24,6 +25,7 @@ describe('Product component', () => {
       keywords: ["socks", "sports", "apparel"]
     };
     loadCart = vi.fn(); // creates a fake function that doesn't do anything
+    user = userEvent.setup();
   });
 
   it('displays the product details correctly', () => {
@@ -54,7 +56,6 @@ describe('Product component', () => {
   it('adds a product to the cart', async () => {
     render(<Product product={product} loadCart={loadCart} />);
 
-    const user = userEvent.setup();
     const addToCartButton = screen.getByTestId('add-to-cart-button')
     await user.click(addToCartButton);
 
@@ -67,5 +68,28 @@ describe('Product component', () => {
     );
 
     expect(loadCart).toHaveBeenCalled();
-  })
+  });
+
+  it('select a quantity', async () => {
+    render(<Product product={product} loadCart={loadCart}></Product>);
+
+    const quantitySelector = screen.getByTestId('product-quantity-selector');
+    expect(quantitySelector).toHaveValue('1');
+
+    await user.selectOptions(quantitySelector, '3');
+    expect(quantitySelector).toHaveValue('3');
+
+    const addToCartButton = screen.getByTestId('add-to-cart-button')
+    await user.click(addToCartButton);
+
+    expect(axios.post).toHaveBeenCalledWith(
+      '/api/cart-items',
+      {
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 3
+      }
+    );
+
+    expect(loadCart).toHaveBeenCalled();
+  });
 });
